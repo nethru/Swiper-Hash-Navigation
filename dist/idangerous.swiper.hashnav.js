82bdcaf6331fdf0a74a63d07cc3bc3a409d2e8c8
Jason Cheung <cheungks1@gmail.com>
2014-08-26 19:47:05 +0800

    prevent adding browser history on change hash

--- dist/idangerous.swiper.hashnav.js
+++ dist/idangerous.swiper.hashnav.js
@@ -10,44 +10,54 @@
  *
  * Licensed under GPL & MIT
  *
- * Released on: January 28, 2014
+ * Released on: August 26, 2014
 */
-Swiper.prototype.plugins.hashNav = function(swiper, params) {
-  var isH = swiper.params.mode == 'horizontal';
-  if(!params) return;
+(function (Swiper) {
 
-  function updateHash(internal){
-    var newHash = swiper.activeSlide().getAttribute('data-hash')
-    if (!newHash) newHash = '';
-    document.location.hash = newHash;
-  }
-  function swipeToHash(e){
-    var hash = document.location.hash;
-    if (!hash) return;
-    var hash = hash.replace('#','');
-    var speed = e ? swiper.params.speed : 0;
-    for (var i=0; i<swiper.slides.length; i++) {
-      var slide = swiper.slides[i];
-      var slideHash = slide.getAttribute('data-hash');
-      if (slideHash == hash && slide.getData('looped')!==true) {
-          var index = slide.index()
-          if (swiper.params.loop) index = index - swiper.loopedSlides;
-          swiper.swipeTo(index, speed);
-      }
-    }
-  }
+    Swiper.prototype.plugins.hashNav = function (swiper, params) {
+        'use strict';
+    
+        var isH = swiper.params.mode === 'horizontal';
+        if (!params) return;
+    
+        function updateHash(internal) {
+            var newHash = '#' + swiper.activeSlide().getAttribute('data-hash') || '';
+    
+            if (history && history.replaceState) {
+                history.replaceState(undefined, undefined, newHash);
+            } else {
+                location.replace(newHash);
+            }
+        }
+    
+        function swipeToHash(e) {
+            var hash = document.location.hash.replace('#', '');
+            if (!hash) return;
+            var speed = e ? swiper.params.speed : 0;
+            for (var i = 0, length = swiper.slides.length; i < length; i++) {
+                var slide = swiper.slides[i];
+                var slideHash = slide.getAttribute('data-hash');
+                if (slideHash === hash && slide.getData('looped') !== true) {
+                    var index = slide.index();
+                    if (swiper.params.loop) index = index - swiper.loopedSlides;
+                    swiper.swipeTo(index, speed);
+                }
+            }
+        }
+    
+        //Plugin Hooks
+        return {
+            onSwiperCreated : function (args) {
+                swipeToHash();
+            },
+            onSlideChangeStart: function () {
+                updateHash(true);
+            },
+            onSwipeReset: function () {
+                updateHash(true);
+            }
+        };
+    };
+    
 
-  //Plugin Hooks
-  var hooks = {
-    onSwiperCreated : function(args){
-      swipeToHash()
-    },
-    onSlideChangeStart: function(){
-      updateHash(true)
-    },
-    onSwipeReset: function(){
-      updateHash(true)
-    }
-  }
-  return hooks
-}
+})(Swiper);
